44ffbb1555ccaa40c59b8724a48ac68900d592d1
AmritpalChera <cheraamritpal@gmail.com>
2023-04-25 09:57:53 -0400

    fix auth handler

--- src/utils/authHandler.ts
+++ src/utils/authHandler.ts
@@ -5,7 +5,7 @@ import requestTracker from './analytics/requestTracker';
 // validates the token and returns a boolean
 export default async function authHandler(req: NextApiRequest) {
   const authHeader = req.headers.authorization;
-  if (!authHeader) return null;
+  if (!authHeader)  throw "Invalid authorization";;
   // the token is the mindplug key
   const token = authHeader!.split(' ')[1];
   const {data, error}= await supabase.from('keys').select().eq('mindplugKey', token);
