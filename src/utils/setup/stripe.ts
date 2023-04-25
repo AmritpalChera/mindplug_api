c70a7e45127802b95290548dfc7f07285bca58b4
AmritpalChera <cheraamritpal@gmail.com>
2023-04-25 00:38:08 -0400

    switch stripe to production mode

--- src/utils/setup/stripe.ts
+++ src/utils/setup/stripe.ts
@@ -1,6 +1,6 @@
 import Stripe from "stripe";
 
-const key = process.env.STRIPE_KEY!;
+const key = process.env.STRIPE_KEY_PROD!;
 const stripe = new Stripe(key, { apiVersion: '2022-11-15' });
 
 export default stripe;
