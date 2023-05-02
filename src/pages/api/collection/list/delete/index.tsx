1762d2f1f56f918f234dbb715ecfb6b9a1b94195
AmritpalChera <cheraamritpal@gmail.com>
2023-05-02 13:25:55 -0400

    delete collection endpoint

---
+++ src/pages/api/collection/list/delete/index.tsx
@@ -0,0 +1,66 @@
+// lists the namespaces for the user given a db (index)
+
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import type { NextApiRequest, NextApiResponse } from 'next';
+import { object, string, TypeOf } from "zod";
+import authHandler from '@/utils/authHandler';
+import initializePinecone from '@/utils/setup/pinecone';
+
+type Data = {
+  success?: boolean
+  error?: string
+}
+
+const bodySchema = object({
+  db: string(),
+  namespace: string()
+})
+
+interface FetchRequest extends NextApiRequest {
+  body: TypeOf<typeof bodySchema>
+}
+
+export default async function handler(req: FetchRequest, res: NextApiResponse<Data>) {
+  
+
+  if (req.method === 'POST') {
+
+    // Extract token
+    let userData;
+    try {
+      userData = await authHandler(req);
+    } catch (e: any) {
+      return res.status(403).json({error: e})
+    }
+   
+    if (!userData) return res.status(403).json({ error: 'Invalid auth' });
+
+    // parse request
+    const result = bodySchema.safeParse(req.body);
+    if (!result.success) return res.status(400).send({error: 'Invalid required parameters', success: false});
+
+    // Generate embeddings and store data to pinecone. Return the stored data _id from Supabase or MongoDB
+    const { db, namespace } = req.body;
+
+    try {
+      const pinecone = await initializePinecone(userData.pineconeKeyEnv, userData.pineconeKey);
+
+      const index = pinecone.Index(db);
+      await index.delete1({
+        deleteAll: true,
+        namespace: namespace
+      });
+      return res.status(200).json({ success: true });
+
+    } catch (e) {
+      console.log(e)
+      return res.status(500).json({ error: `unable to create index: ${e}` });
+    }
+    
+  } else {
+    res.status(400).json({ error: 'Invalid request' })
+  }
+
+    
+  
+}
