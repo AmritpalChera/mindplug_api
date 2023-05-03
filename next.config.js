539902e6cab2b5d6404daa10729c9c7bae6addb5
AmritpalChera <cheraamritpal@gmail.com>
2023-05-02 22:37:30 -0400

    add headers

--- next.config.js
+++ next.config.js
@@ -9,6 +9,20 @@ const nextConfig = {
 
     return config;
   },
+  async headers() {
+    return [
+      {
+        // matching all API routes
+        source: "/api/:path*",
+        headers: [
+          { key: "Access-Control-Allow-Credentials", value: "true" },
+          { key: "Access-Control-Allow-Origin", value: "*" },
+          { key: "Access-Control-Allow-Methods", value: "GET,POST" },
+          { key: "Access-Control-Allow-Headers", value: "Content-Type, Authorization, X-Requested-With" },
+        ],
+      }
+    ]
+  }
 }
 
 module.exports = nextConfig
