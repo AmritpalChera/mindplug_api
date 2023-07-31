03907c5a5c2d186dc0b1f046cc99bf035b0246d9
AmritpalChera <cheraamritpal@gmail.com>
2023-07-30 22:18:00 -0400

    fix web scraper

--- src/utils/webParsers/loadWebpage.ts
+++ src/utils/webParsers/loadWebpage.ts
@@ -1,17 +1,11 @@
-import { chromium } from 'playwright-chromium';
-
+import axios from 'axios';
 const loadWebContent = async (url: string) => {
-  const browser = await chromium.launch({headless: true}); 
-  const context = await browser.newContext({ userAgent: 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)' +
-  ' AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36', });
-  const page = await context.newPage(); 
-    // Navigate to a website 
-  await page.goto(url); 
-  //   // Do something on the website
-    // ... 
-  const textContents = await page.innerText('body');
-  await browser.close(); 
-  return textContents.replace(/[\r\n]+/gm, " ");
+  const textContents = await axios.post('https://us-central1-experai.cloudfunctions.net/function-1', { url }, {
+    headers: {
+      "Content-Type": "application/json"
+    }
+  }).then(res => res.data);
+  return textContents;
 }
 
 export default loadWebContent;
