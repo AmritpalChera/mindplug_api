14fcc2ec9f36c7a02784b62dfb3de5a718ff4f99
AmritpalChera <cheraamritpal@gmail.com>
2023-07-30 22:34:48 -0400

    revert to gcp scraper

--- src/utils/webParsers/loadWebpage.ts
+++ src/utils/webParsers/loadWebpage.ts
@@ -1,29 +1,11 @@
 import axios from 'axios';
-import { chromium } from 'playwright-core';
-import bundledChromium from 'chrome-aws-lambda';
-
-
 const loadWebContent = async (url: string) => {
-  const browser = await Promise.resolve(bundledChromium.executablePath).then(
-    (executablePath) => {
-      if (!executablePath) {
-        // local execution
-        return chromium.launch({});
-      }
-      return chromium.launch({ executablePath });
+  const textContents = await axios.post('https://us-central1-experai.cloudfunctions.net/function-1', { url }, {
+    headers: {
+      "Content-Type": "application/json"
     }
-  ); 
-  const context = await browser.newContext({ userAgent: 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)' +
-  ' AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36', });
-  const page = await context.newPage(); 
-    // Navigate to a website 
-  await page.goto((url || 'https://mindplug.io')); 
-  //   // Do something on the website
-    // ... 
-  const textContents = await page.innerText('body');
-  await browser.close(); 
-  const toReturn = textContents.replace(/[\r\n]+/gm, " ");
-  return toReturn;
+  })
+  return textContents;
 }
 
 export default loadWebContent;
