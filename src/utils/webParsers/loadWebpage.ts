99c3f12e861816c42c8df5dcd24fb66d71099a44
AmritpalChera <cheraamritpal@gmail.com>
2023-07-30 22:21:32 -0400

    try raw

--- src/utils/webParsers/loadWebpage.ts
+++ src/utils/webParsers/loadWebpage.ts
@@ -1,11 +1,29 @@
 import axios from 'axios';
+const { chromium } = require('playwright-core');
+const bundledChromium = require('chrome-aws-lambda');
+
+
 const loadWebContent = async (url: string) => {
-  const textContents = await axios.post('https://us-central1-experai.cloudfunctions.net/function-1', { url }, {
-    headers: {
-      "Content-Type": "application/json"
+  const browser = await Promise.resolve(bundledChromium.executablePath).then(
+    (executablePath) => {
+      if (!executablePath) {
+        // local execution
+        return chromium.launch({});
+      }
+      return chromium.launch({ executablePath });
     }
-  }).then(res => res.data);
-  return textContents;
+  ); 
+  const context = await browser.newContext({ userAgent: 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)' +
+  ' AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36', });
+  const page = await context.newPage(); 
+    // Navigate to a website 
+  await page.goto((url || 'https://mindplug.io')); 
+  //   // Do something on the website
+    // ... 
+  const textContents = await page.innerText('body');
+  await browser.close(); 
+  const toReturn = textContents.replace(/[\r\n]+/gm, " ");
+  return toReturn;
 }
 
 export default loadWebContent;
